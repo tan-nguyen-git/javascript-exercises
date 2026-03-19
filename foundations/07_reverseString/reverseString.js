7f07393c7ca45190a46d3ad58a0d2c4f4c6623db
tan-nguyen-git <thanhtandanang15@gmail.com>
2026-03-19 17:31:22 -0500

    js foudation exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    str = str.split("");
+    let n = str.length;
+    
+    console.log(n);
+    for(let i =0; i < Math.floor(n/2);i++){
+        let j = n-i-1;
+        console.log(i);
+        console.log(j);
+        let temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    return str.join("");
 
 };
 
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
