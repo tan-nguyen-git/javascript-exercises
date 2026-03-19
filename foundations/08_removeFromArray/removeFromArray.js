82d1d84c7042491853514d6c8494381a51569500
tan-nguyen-git <thanhtandanang15@gmail.com>
2026-03-19 17:45:52 -0500

    js foudation 09

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const helper = (e, arr)=>{
+    return arr.find(x=>x ===e);
+}
+
+const removeFromArray = function(arr, ...toFilter) {
+    return arr.filter(e=> !helper(e,toFilter));
 };
+
+
+console.log(removeFromArray([1,2,3], 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
