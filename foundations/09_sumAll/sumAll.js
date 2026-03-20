d09d934169f1b17158d9ac79bad1ec8ba4c79a44
tan-nguyen-git <thanhtandanang15@gmail.com>
2026-03-19 21:58:23 -0500

    09 fix

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,14 +2,10 @@ const sumAll = function(a,b) {
     if(typeof a ==="number" && typeof a ==="number" ){
         if(a<0 || b < 0 || !Number.isInteger(a) ||!Number.isInteger(b) ) return "ERROR";
         let space = Math.abs(a-b)+1;
-        console.log(space);
         let m = Math.min(a,b);
-        console.log(m);
-
         let res = 0;
         for(let i = 0 ; i < space ; i++){
             res += (m+i);
-        
         }
         return res;
 
